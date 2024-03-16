68887ac63054c5b9638ed2162c467ecc8ef69142
Elmin Gulamov <g.elmn@yandex.ru>
2024-03-16 23:35:21 +0400

    pytest

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+from selene import browser, be, have
+import time
+
+
+def test_open_google():
+    browser.open("https://www.google.com")
+    browser.element('[name="q"]').should(be.blank).type('Россия').press_enter()
+    browser.element('[id="search"]').should(have.text('Ро1ссия'))
+
+
