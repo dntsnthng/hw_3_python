ed5a22826f7c66be90f2e9fff6bd638fecd7c08f
Elmin Gulamov <g.elmn@yandex.ru>
2024-03-17 21:20:16 +0400

    new3

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ from selene import browser, be, have
 
 def test_open_google():
     browser.open("https://www.google.com")
-    browser.element('[name="q"]').should(be.blank).type('Россия').press_enter()
-    browser.element('[id="search"]').should(have.text('Ро1ссия'))
+    browser.element('[name="q"]').should(be.blank).type('jfdhgkjdfhgjkfdhgfdkg').press_enter()
+    browser.element('[id="search"]').should(have.text('Как стать супергероям за 24 часа без смс и регистрации'))
+
 
